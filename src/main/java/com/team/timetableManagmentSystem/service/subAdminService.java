487f248b647e53351dd27c0c5c3d343a39c24451
Mostafa <Mostafa@10.23.2.112>
2024-06-14 16:33:47 +0300

    update

--- src/main/java/com/team/timetableManagmentSystem/service/subAdminService.java
+++ src/main/java/com/team/timetableManagmentSystem/service/subAdminService.java
@@ -1,17 +1,22 @@
 package com.team.timetableManagmentSystem.service;
 
-import com.team.timetableManagmentSystem.DTOs.*;
+import com.team.timetableManagmentSystem.DTOs.Branch;
 import com.team.timetableManagmentSystem.database.connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
 public class subAdminService {
-
     
-
+    public Object getTimetablesToView(ArrayList<Branch> branches) {
+        connection conn = new connection();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < branches.size(); i++) {
+            sb.append(branches.get(i).getId() + ",");
+        }
+        
+        ResultSet rs = conn.select("select distinct timetable.id,timetable.name from timetable where timetable.id in (select timesintimetable.timeTableId from timesintimetable  where timesintimetable.hostingBranchID in (" + sb.substring(0, sb.length() - 1) + ") or timesintimetable.id in (select branchesintimeintimetable.timeInTimetableId from branchesintimeintimetable where branchesintimeintimetable.branchId=13 ));");
+        return null;
+    }
 }
