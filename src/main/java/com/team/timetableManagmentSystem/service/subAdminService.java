9f3c772165438507935e6f3f05a74c7110516292
Mostafa <Mostafa@10.23.2.112>
2024-06-14 19:29:22 +0300

    update

--- src/main/java/com/team/timetableManagmentSystem/service/subAdminService.java
+++ src/main/java/com/team/timetableManagmentSystem/service/subAdminService.java
@@ -8,15 +8,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class subAdminService {
-    
+
     public Object getTimetablesToView(ArrayList<Branch> branches) {
         connection conn = new connection();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < branches.size(); i++) {
             sb.append(branches.get(i).getId() + ",");
         }
+
+        ResultSet rs = conn.select("select distinct timetable.id,timetable.name from timetable where timetable.id in (select timesintimetable.timeTableId from timesintimetable  where timesintimetable.hostingBranchID in (" + sb.substring(0, sb.length() - 1) + ") or timesintimetable.id in (select branchesintimeintimetable.timeInTimetableId from branchesintimeintimetable where branchesintimeintimetable.branchId in (" + sb.substring(0, sb.length() - 1) + ") ));");
         
-        ResultSet rs = conn.select("select distinct timetable.id,timetable.name from timetable where timetable.id in (select timesintimetable.timeTableId from timesintimetable  where timesintimetable.hostingBranchID in (" + sb.substring(0, sb.length() - 1) + ") or timesintimetable.id in (select branchesintimeintimetable.timeInTimetableId from branchesintimeintimetable where branchesintimeintimetable.branchId=13 ));");
         return null;
     }
 }
